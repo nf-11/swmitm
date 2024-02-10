779638ec575ccce9efd3de22990f7ebffd6e5bb0
nf-11 <147265258+nf-11@users.noreply.github.com>
2024-02-10 03:23:45 +0200

    Add proxy_mode and proxy_port

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     subprocess.run([config.get('MITM', 'mitmdump_executable_path'),
                     '--allow-hosts', config.get('MITM', 'allowed_hosts'),
                     '--scripts', pathlib.Path(__file__).parent / 'swmitm' / 'mitm.py',
+                    '--mode', config.get('Settings', 'proxy_mode'),
+                    '--listen-port', config.get('Settings', 'proxy_port'),
                     '-q'])
 
 
